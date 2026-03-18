31d1a4fd4dfb0f2c219bb5dd247f201ed86eeb2f
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-03-18 20:51:41 -0300

    :camel: feat(Domain): Add Driver entity

---
+++ FleetTracker/Domain/Entities/Driver.cs
@@ -0,0 +1,34 @@
+﻿namespace FleetTracker.Domain.Entities;
+
+public class Driver : EntityBase
+{
+    public string name { get; private set; }
+    public string email { get; private set; }
+    public string password { get; private set; }
+    public string document { get; private set; }
+    public string department { get; private set; }
+
+    public Driver(string name, string email, string password,
+        string document, string department)
+    {
+        Dictionary<string, string> parameters = new Dictionary<string, string>
+        {
+            {"name", name},
+            {"email", email},
+            {"password", password},
+            {"document", document},
+            {"department", department}
+        };
+        foreach (var parameter in parameters)
+        {
+            if (string.IsNullOrEmpty(parameter.Value))
+                throw new ArgumentException(parameter.Key + " must not be null or empty");
+        }
+        
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.document = document;
+        this.department = department;
+    }
+}
