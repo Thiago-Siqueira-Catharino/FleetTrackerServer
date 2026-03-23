21271fbc349341f599608924edf6ca46c784400c
Thiago Siqueira <thiago.s.catharino@gmail.com>
2026-03-23 17:08:48 -0300

    :alien: Feat(Domain): Create method to append new elements to locationPoints within Path entity

--- FleetTracker/Domain/Entities/Path.cs
+++ FleetTracker/Domain/Entities/Path.cs
@@ -3,5 +3,23 @@
 public class Path
 {
     public Car car { get; private set;  }
-    public List<LocationPoint> locationPoints { get; private set; }
+    private List<LocationPoint> locationPoints { get; set; } = new List<LocationPoint>();
+
+    public Path(Car car)
+    {
+        this.car = car;
+    }
+
+    public void AddLocationPoint(LocationPoint locationPoint)
+    {
+        if (locationPoint is null)
+        {
+            throw new ArgumentException("Invalid location point");
+        }
+
+        if(locationPoints.Count > 0 && locationPoint.timeStamp <= locationPoints.Last().timeStamp)
+            throw new ArgumentException("Invalid timestamp, time only moves foward");
+        
+        locationPoints.Add(locationPoint);
+    }
 }
