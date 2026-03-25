05a7d9ca500843247e17d23180efe4e7f53afd28
Thiago Siqueira <thiago.s.catharino@gmail.com>
2026-03-25 11:08:01 -0300

    :alien: feat(Domain): Add new DTO for location point creation

--- FleetTracker/Domain/DTOs/Requests/CreateLocationPointDTO.cs
+++ FleetTracker/Domain/DTOs/Requests/CreateLocationPointDTO.cs
@@ -1,19 +1,30 @@
 namespace FleetTracker.Domain.DTOs.Requests;
 
-public class CreateCoodinateDTO
+public class CreateLocationPointDTO
 {
+    public DateTime timestamp { get; set; }
     public double latitude { get; set; }
     public double longitude { get; set; }
+    public double fuelLevel { get; set; }
 
-    public CreateCoodinateDTO(double latitude, double longitude)
+    public CreateLocationPointDTO(DateTime timestamp, double latitude, double longitude, double fuelLevel)
     {
-        if (String.IsNullOrEmpty(latitude.ToString()))
-            throw new ArgumentException("latitude must not be null or empty");
+        Dictionary<string, string> parameters = new Dictionary<string, string>
+        {
+            {"timestamp", timestamp.ToString()},
+            {"latitude", latitude.ToString()},
+            {"longitude", longitude.ToString()},
+            {"fuel level", fuelLevel.ToString()}
+        };
+        foreach (var parameter in parameters)
+        {
+            if (string.IsNullOrEmpty(parameter.Value))
+                throw new ArgumentException($"{parameter.Key} must not be null or empty");
+        }
         
-        if (String.IsNullOrEmpty(longitude.ToString()))
-            throw new ArgumentException("longitude must not be null or empty");
-        
+        this.timestamp = timestamp;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.fuelLevel = fuelLevel;
     }
 }
