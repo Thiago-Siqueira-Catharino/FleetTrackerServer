037e634231146dc73b8467bbe58246a315366e80
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-04-08 15:24:49 -0300

    Feat(Car): New attributes and related methods
    attributes:
    - CreatedAt
    - RemovedAt
    - Active
    
    methods:
    - Remove
    - Deactivate

--- FleetTracker/Contexts/Fleet/Domain/Entities/Car.cs
+++ FleetTracker/Contexts/Fleet/Domain/Entities/Car.cs
@@ -2,14 +2,32 @@
 
 public class Car
 {
+    public Guid Id { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime RemovedAt { get; private set; }
+    public bool Active { get; private set; }
     public string TagUid { get; }
     public string Model { get; set; }
     public string Plate { get; set; }
 
-    public Car(string tagUid, string model, string plate, float averageConsumption)
+    public Car(string tagUid, string model, string plate)
     {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.Now;
         TagUid = tagUid;
         Model = model;  
         Plate = plate;
+        Active = true;
+    }
+
+    public void Deactivate()
+    {
+        Active = false;
+    }
+
+    public void Remove()
+    {
+        Active = false;
+        RemovedAt = DateTime.Now;
     }
 }
