3d59f278ab273c560f0ae46169871933d78ec6e9
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-04-08 15:55:09 -0300

    feat(usecases): new usecase to retrieve Car obj from db by id

---
+++ FleetTracker/Contexts/Fleet/Application/UseCases/GetCarById/GetCarByIdUseCase.cs
@@ -0,0 +1,19 @@
+﻿using FleetTracker.Contexts.Fleet.Domain.Entities;
+using FleetTracker.Contexts.Fleet.Domain.Repositories;
+
+namespace FleetTracker.Contexts.Fleet.Application.UseCases.GetCarById;
+
+public class GetCarByIdUseCase
+{
+    ICarRepository _carRepository;
+
+    public GetCarByIdUseCase(ICarRepository carRepository)
+    {
+        _carRepository = carRepository;
+    }
+
+    public Car Run(GetCarByIdDTO id)
+    {
+        return _carRepository.FindById(id.value);
+    }
+}
