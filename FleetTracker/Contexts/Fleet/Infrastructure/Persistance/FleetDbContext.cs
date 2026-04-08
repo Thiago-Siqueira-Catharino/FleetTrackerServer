d7bd140b29c306c7e629e570c4c63cb7a0d1dfc9
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-04-08 15:33:23 -0300

    feat(persistance): introduce mapping for Car entity

--- FleetTracker/Contexts/Fleet/Infrastructure/Persistance/FleetDbContext.cs
+++ FleetTracker/Contexts/Fleet/Infrastructure/Persistance/FleetDbContext.cs
@@ -1,6 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using FleetTracker.Contexts.Fleet.Domain.Entities;
+
 namespace FleetTracker.Contexts.Fleet.Infrastructure.Persistance;
 
-public class FleetDbContext
+public class FleetDbContext : DbContext
 {
-    
+    public DbSet<Car> Cars { get; set; }
+
+    public FleetDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Car>()
+            .HasKey(car => car.Id);
+
+        modelBuilder.Entity<Car>()
+            .HasAlternateKey(car => car.Plate);
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
