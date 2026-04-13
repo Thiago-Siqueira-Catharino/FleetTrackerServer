8f7c9eeeda3c8ced736331b8ee58b415268c4582
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-04-13 20:18:16 -0300

    :alien: feat(auth): basic dbcontext setup

--- FleetTracker/Contexts/Auth/Infrastructure/Persistance/AuthDbContext.cs
+++ FleetTracker/Contexts/Auth/Infrastructure/Persistance/AuthDbContext.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace FleetTracker.Contexts.Auth.Infrastructure.Persistance;
 
-public class AuthDbContext
+public class AuthDbContext : IdentityDbContext<IdentityUser>
 {
-    
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+    }
 }
