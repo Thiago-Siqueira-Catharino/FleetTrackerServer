829f7794fe9b772627d89b52b21504bedd377690
Luan Girotto <luan.girotto.am@gmail.com>
2026-04-13 22:35:50 -0300

    Use Cases Fleet (incompleto)

---
+++ FleetTracker/Contexts/Fleet/Application/UseCases/RegisterCarLocation/RegisterCarLocationUseCase.cs
@@ -0,0 +1,37 @@
+﻿using FleetTracker.Contexts.Fleet.Domain.Repositories;
+using FleetTracker.Domain.Entities;
+
+namespace FleetTracker.Contexts.Fleet.Application.UseCases.RegisterCarLocation
+{
+    public class RegisterCarLocationUseCase
+    {
+        private readonly ILocationRepository _locationRepository; //Precisa criar ainda
+        private readonly ICarRepository _carRepository;
+
+        public RecordCarLocationUseCase(ILocationRepository locationRepository, ICarRepository carRepository)
+        {
+            _locationRepository = locationRepository;
+            _carRepository = carRepository;
+        }
+
+        public async Task ExecuteAsync(RecordCarLocationDTO request)
+        {
+            var car = await _carRepository.GetByIdAsync(request.CarId);
+            if (car == null)
+            {
+                throw new Exception("Carro não encontrado.");
+            }
+
+            var coordinate = new Coordinate(request.Latitude, request.Longitude);
+
+            var locationPoint = new LocationPoint(
+                coordinate: coordinate,
+                timestamp: request.Timestamp,
+                driverId: request.DriverId,
+                carId: request.CarId
+            );
+
+            await _locationRepository.AddAsync(locationPoint);
+        }
+    }
+}
