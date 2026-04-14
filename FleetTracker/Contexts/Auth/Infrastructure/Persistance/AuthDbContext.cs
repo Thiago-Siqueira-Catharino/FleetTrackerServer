f6d342f4bba4adb7ce395cf3ff56b4c762ade7b6
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-04-14 14:29:16 -0300

    update(dbcontext): setup context entities

--- FleetTracker/Contexts/Auth/Infrastructure/Persistance/AuthDbContext.cs
+++ FleetTracker/Contexts/Auth/Infrastructure/Persistance/AuthDbContext.cs
@@ -1,11 +1,14 @@
-using Microsoft.AspNetCore.Identity;
+using FleetTracker.Contexts.Auth.Domain.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FleetTracker.Contexts.Auth.Infrastructure.Persistance;
 
-public class AuthDbContext : IdentityDbContext<IdentityUser>
+public class AuthDbContext : IdentityDbContext<User>
 {
+    public DbSet<Admin> Administrators { get; set; }
+    public DbSet<Driver> Drivers { get; set; }
+    public DbSet<FieldAgent> FieldAgents { get; set; }
     public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
     {
     }
