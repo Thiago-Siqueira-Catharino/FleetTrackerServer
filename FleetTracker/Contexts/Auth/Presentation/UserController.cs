e80048e7dd366cfabace50a34357bad3f9d8033c
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-04-15 14:58:18 -0300

    :alien: feat(presentation): adds controller with login and sign in endpoints

--- FleetTracker/Contexts/Auth/Presentation/UserController.cs
+++ FleetTracker/Contexts/Auth/Presentation/UserController.cs
@@ -1,6 +1,48 @@
+using FleetTracker.Contexts.Auth.Application.UseCases.LoginUseCase;
+using FleetTracker.Contexts.Auth.Application.UseCases.NewUserUseCase;
+using FleetTracker.Contexts.Auth.Application.UseCases.NewUserUseCase.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
 namespace FleetTracker.Contexts.Auth.Presentation;
 
-public class UserController
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(
+    NewDriverUseCase registerDriver, 
+    NewAdminUseCase registerAdmin, 
+    NewFieldAdgentUseCase registerAgent, 
+    LoginUseCase loginUseCase
+    ) : ControllerBase
 {
+    [HttpPost("register/driver")]
+    public async Task<IActionResult> RegisterDriver(NewDriverDto driverToRegister)
+    {
+        await registerDriver.Run(driverToRegister);
+        return Ok();
+    }
     
+    [HttpPost("register/admin")]
+    public async Task<IActionResult> RegisterAdmin(NewUserDto newUser)
+    {
+        await registerAdmin.Run(newUser);
+        return Ok();
+    }
+
+    [HttpPost("register/fieldagent")]
+    public async Task<IActionResult> RegisterFieldAgent(NewUserDto newUser)
+    {
+        await registerAgent.Run(newUser);
+        return Ok();
+    }
+    
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginDto login)
+    {
+        var token = await loginUseCase.Run(login);
+        
+        if  (token == null)
+            return  Unauthorized();
+        
+        return Ok(token);
+    }
 }
