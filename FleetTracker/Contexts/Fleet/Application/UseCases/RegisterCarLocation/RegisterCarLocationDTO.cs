92a2fcbfea4634b30f72e4e2438252450fe48f27
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-04-15 15:44:00 -0300

    :alien: fix(registerCarlocationdto): add missing fields

--- FleetTracker/Contexts/Fleet/Application/UseCases/RegisterCarLocation/RegisterCarLocationDTO.cs
+++ FleetTracker/Contexts/Fleet/Application/UseCases/RegisterCarLocation/RegisterCarLocationDTO.cs
@@ -6,6 +6,7 @@
         public Guid DriverId { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public double FuelLevel { get; set; }
         public DateTime Timestamp { get; set; }
     }
 }
