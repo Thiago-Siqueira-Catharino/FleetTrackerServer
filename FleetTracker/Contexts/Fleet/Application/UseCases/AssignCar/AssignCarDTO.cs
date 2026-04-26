7ecc88e176a2736f96e8d85841d21bfdd6235120
Luan Girotto <luan.girotto.am@gmail.com>
2026-04-26 18:04:22 -0300

    AssignCarUseCase / DTO

---
+++ FleetTracker/Contexts/Fleet/Application/UseCases/AssignCar/AssignCarDTO.cs
@@ -0,0 +1,11 @@
+﻿using FleetTracker.Contexts.Auth.Domain.Entities;
+using FleetTracker.Contexts.Fleet.Domain.Entities;
+
+namespace FleetTracker.Contexts.Fleet.Application.UseCases.AssignCar
+{
+    public class AssignCarDTO
+    {
+        public Driver DriverId { get; set; } //Id do Motorista é um objeto DocumentoCNH? Se for, não sei como chamar aqui para estar vinculado a um Driver
+        public Guid CarId { get; set; } //Carro tem várias properties que podem ser consideradas IDs, como LicensePlate, TagUID e Id, peguei o ID simples
+    }
+}
