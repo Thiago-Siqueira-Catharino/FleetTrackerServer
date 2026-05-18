c19cc3a936db0bd7e94bbc46b53b1f5950525445
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-05-18 12:33:28 -0300

    feat: configure coordinate owned entity for locationpoint

--- FleetTracker/Contexts/Telemetry/Infrastructure/Persistance/TelemetryDbContext.cs
+++ FleetTracker/Contexts/Telemetry/Infrastructure/Persistance/TelemetryDbContext.cs
@@ -9,7 +9,7 @@ public class TelemetryDbContext  : DbContext
     public DbSet<LocationPoint> LocationPoints { get; set; }
     public DbSet<Domain.Entities.Path> Paths { get; set; }
 
-    public TelemetryDbContext(DbContextOptions<TelemetryDbContext> options)
+    public TelemetryDbContext(DbContextOptions<TelemetryDbContext> options) : base(options)
     {
     }
 
@@ -22,6 +22,13 @@ public class TelemetryDbContext  : DbContext
             .HasOne<Domain.Entities.Path>()
             .WithMany()
             .HasForeignKey(point => point.pathId);
+
+        modelBuilder.Entity<LocationPoint>()
+            .OwnsOne(lp => lp.coordinate, coordinate =>
+            {
+                coordinate.Property(c => c.latitude).HasColumnName("latitude");
+                coordinate.Property(c => c.longitude).HasColumnName("longitude");
+            });
         
         modelBuilder.Entity<Domain.Entities.Path>()
             .HasKey(path => path.Id);
