77c93633932477967f80528d6b953a1269953de1
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-06-01 15:33:27 -0300

    feat: new path search method, filtering by carId field

--- FleetTracker/Contexts/Telemetry/Domain/Repositories/IPathRepository.cs
+++ FleetTracker/Contexts/Telemetry/Domain/Repositories/IPathRepository.cs
@@ -7,5 +7,6 @@ public interface IPathRepository
     public Task AddAsync(Entities.Path pathToSave);
     public Task UpdateAsync(Entities.Path pathToUpdate);
     public Task<Entities.Path?> FindByIdAsync(Guid pathId);
+    public Task<IEnumerable<Entities.Path>> FindByCarIdAsync(Guid carId);
     
 }
