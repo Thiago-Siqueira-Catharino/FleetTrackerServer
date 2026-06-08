41a0336f09bca73c4997096ddaca8e154663962b
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-06-08 00:39:09 -0300

    feat: new endpoints to fetch all paths, and fetch paths filtered by car Id

--- FleetTracker/Contexts/Telemetry/Presentation/PathController.cs
+++ FleetTracker/Contexts/Telemetry/Presentation/PathController.cs
@@ -1,4 +1,6 @@
 using FleetTracker.Contexts.Telemetry.Application.UseCases.CreatePathUseCase;
+using FleetTracker.Contexts.Telemetry.Application.UseCases.ListPathByCarIdUseCase;
+using FleetTracker.Contexts.Telemetry.Application.UseCases.ListPathsUseCase;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,13 +8,28 @@ namespace FleetTracker.Contexts.Telemetry.Presentation;
 
 [ApiController]
 [Route("api/[controller]")]
-public class PathController(CreatePathUseCase createPathUseCase) : ControllerBase
+public class PathController(
+    CreatePathUseCase createPathUseCase,
+    ListPathByCarIdUseCase listPathByCarIdUseCase,
+    ListPathsUseCase listPathsUseCase
+    ) : ControllerBase
 {
     [Authorize(Roles = "Driver")]
     [HttpPost]
     public async Task<IActionResult> StartPath(CreatePathDto dto)
     {
-        await createPathUseCase.RunAsync(dto);
-        return Ok();
+        return Ok(await createPathUseCase.RunAsync(dto));
+    }
+
+    [HttpGet("search/id={carId}")]
+    public async Task<IActionResult> ListPathsByCarId(Guid carId)
+    {
+        return Ok(await listPathByCarIdUseCase.RunAsync(carId));
+    }
+
+    [HttpGet("search/")]
+    public async Task<IActionResult> ListPaths()
+    {
+        return Ok(await listPathsUseCase.RunAsync());
     }
 }
