25861327cddbe369fce4a33b668ad8f5de7fba0a
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-06-08 00:38:09 -0300

    feat: new endpoint to fetch locationPoints filtered by path Id

--- FleetTracker/Contexts/Telemetry/Presentation/LocationPointController.cs
+++ FleetTracker/Contexts/Telemetry/Presentation/LocationPointController.cs
@@ -1,3 +1,4 @@
+using FleetTracker.Contexts.Telemetry.Application.UseCases.ListLocationPointByPathIdUseCase;
 using FleetTracker.Contexts.Telemetry.Application.UseCases.RegisterCarLocation;
 using FleetTracker.Contexts.Telemetry.Application.UseCases.RegisterCarLocation;
 using Microsoft.AspNetCore.Authorization;
@@ -7,13 +8,23 @@ namespace FleetTracker.Contexts.Telemetry.Presentation;
 
 [ApiController]
 [Route("api/[controller]")]
-public class LocationPointController (RegisterCarLocationUseCase useCase) : ControllerBase
+public class LocationPointController (
+    RegisterCarLocationUseCase useCase,
+    ListLocationPointByPathUseCase listLocationPointByPathUseCase
+    ): ControllerBase
 {
     [Authorize(Roles = "Driver")]
     [HttpPost("location/update")]
     public async Task<IActionResult> UpdateLocation(RegisterCarLocationDTO dto)
     {
+        Console.WriteLine(dto.Latitude + " DEBUGGER MESSAGE " + dto.Longitude);
         await useCase.RunAsync(dto);
         return Ok();
+    }
+
+    [HttpGet("location/search/pathId={pathId}")]
+    public async Task<IActionResult> SearchPathId(Guid pathId)
+    {
+        return Ok(await listLocationPointByPathUseCase.RunAsync(pathId));
     }
 }
